e72227fb4c446663de3e4d60f766e5a63b2b6b4f
hellcoldby <coldbao@gmail.com>
2020-03-24 19:40:23 +0800

    添加直线绘制

--- src/graphic/shape/BezierCurve/BezierCurve.js
+++ src/graphic/shape/BezierCurve/BezierCurve.js
@@ -6,9 +6,9 @@ import Path from "../../Path/Path";
 import { merge } from "../../../tools/data_util";
 let defaultConfig = {
     shape: {
-        x1: 0,
+        x1: 0, // 开始位置
         y1: 0,
-        x2: 0,
+        x2: 0, //结束位置
         y2: 0,
         cpx1: 0,
         cpy1: 0,
@@ -23,6 +23,23 @@ let defaultConfig = {
 export default class BezierCurve extends Path {
     constructor(opts) {
         super(merge(defaultConfig, opts, true));
-        this.type = "arc";
+        this.type = "bezier-curve";
+    }
+
+    buildPath(ctx, shape) {
+        let x1 = shape.x1;
+        let y1 = shape.y1;
+        let x2 = shape.x2;
+        let y2 = shape.y2;
+        let cpx1 = shape.cpx1;
+        let cpy1 = shape.cpy1;
+        let cpx2 = shape.cpx2;
+        let cpy2 = shape.cpy2;
+        let percent = shpae.percent;
+        if (percent === 0) {
+            return;
+        }
+
+        ctx.moveTo(x1, y1);
     }
 }
