cc938465345fcfc0f8d80b6fdef82aec92356f59
hellcoldby <coldbao@gmail.com>
2020-04-01 19:41:05 +0800

    增加文字绘制

--- dist/humble-render.js
+++ dist/humble-render.js
@@ -4375,13 +4375,23 @@
     class TextRender {
         constructor() {}
         drawRectText(ctx, style) {
+            //合并字体样式
             const { fontSize, fontFamily, fontStyle, fontWeight } = style;
             let font = "";
             if (fontSize || fontFamily) {
                 font = `${fontStyle} ${fontWeight} ${fontSize || 12}px ${fontFamily || "sans-serif"}`;
             }
             style.font = style.font || font;
-            console.log(style.font);
+
+            //处理水平位置
+            let textAlign = style.textAlign === "middle" ? "center" : style.textAlign;
+            style.textAlign = ["left", "center", "right"].indexOf(textAlign) === -1 ? "left" : textAlign;
+            //处理垂直位置
+            textVerticalAlign = style.textVerticalAlign || style.textBaseline;
+            let textVerticalAlign = textVerticalAlign === "center" ? "middle" : textVerticalAlign;
+            style.textVerticalAlign = ["top", "bottom", "middle"].indexOf(textVerticalAlign) === -1 ? "top" : textVerticalAlign;
+
+            //处理内边距
         }
     }
 
